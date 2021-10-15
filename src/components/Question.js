d7a6346c129ff7c2edf89c29e5e37ce933d027db
Ehsan_Heidari <ehsan.heydari@gmail.com>
2021-10-15 01:42:30 -0700

    edit jasonFiles

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,8 +2,29 @@ const Question = ({ question, value, onChange }) => {
   return (
     <div>
       <div>{question}</div>
-      <div>
+      {/* <div>
         <input value={value} onChange={onChange} />
+      </div> */}
+      <div>
+        <form>
+          <input
+            type="radio"
+            value={value}
+            id="iOS"
+            onChange={onChange}
+            name="OS"
+          />
+          <label for="iOS">iOS</label>
+
+          <input
+            type="radio"
+            value={value}
+            id="Android"
+            onChange={onChange}
+            name="OS"
+          />
+          <label for="Android">Android</label>
+        </form>
       </div>
     </div>
   );
