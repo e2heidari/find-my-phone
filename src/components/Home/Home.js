82230f2d5a959a052f9c0352acc11a32d248e92f
Ehsan_Heidari <ehsan.heydari@gmail.com>
2021-12-28 00:02:14 -0800

    some change in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import { Main, Background } from "./styled";
 const Home = () => {
   return (
     <Background>
-      <Main>We help you to make the right choice</Main>
+      <Main>We help you to make a right choice</Main>
     </Background>
   );
 };
