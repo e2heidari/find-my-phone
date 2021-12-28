6d982831b60b58196830d799074aeefd8907065d
Ehsan_Heidari <ehsan.heydari@gmail.com>
2021-12-28 02:00:08 -0800

    change compare file

--- src/components/Compare/Comparephone.js
+++ src/components/Compare/Comparephone.js
@@ -49,60 +49,62 @@ const ComparePhone = ({ phones, result1, secondResult }) => {
         />
       </CompareAutocomplete>
 
-      <OutcomeContainer>
-        <Image src={secondResult.image} alt={secondResult.brandName} />
-        <Title>{secondResult.brandName}</Title>
-        <FeatureContainer>
-          <Circle>
-            <Value>{secondResult.os}</Value>
-            <TypeOfValue>OS</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>${secondResult.priceRange}</Value>
-            <TypeOfValue>Price</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.cameraDayScore}</Value>
-            <TypeOfValue>Camera day</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.cameraNightScore}</Value>
-            <TypeOfValue>Camera night</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.cameraVideoScore}</Value>
-            <TypeOfValue>Camera video</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.cameraZoomScore}</Value>
-            <TypeOfValue>Camera zoom</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.cameraSelfieScore}</Value>
-            <TypeOfValue>Camera selfie</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.batteryScore}</Value>
-            <TypeOfValue>Battery</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.audioPlaybackQualityScore}</Value>
-            <TypeOfValue>Audio playback quality</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.audioRecordingQualityScore}</Value>
-            <TypeOfValue>Audio recording quality</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.displayScore}</Value>
-            <TypeOfValue>Display</TypeOfValue>
-          </Circle>
-          <Circle>
-            <Value>{secondResult.performanceScore}</Value>
-            <TypeOfValue>Performance</TypeOfValue>
-          </Circle>
-        </FeatureContainer>
-      </OutcomeContainer>
+      {showup ? (
+        <OutcomeContainer>
+          <Image src={secondResult.image} alt={secondResult.brandName} />
+          <Title>{secondResult.brandName}</Title>
+          <FeatureContainer>
+            <Circle>
+              <Value>{secondResult.os}</Value>
+              <TypeOfValue>OS</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>${secondResult.priceRange}</Value>
+              <TypeOfValue>Price</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.cameraDayScore}</Value>
+              <TypeOfValue>Camera day</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.cameraNightScore}</Value>
+              <TypeOfValue>Camera night</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.cameraVideoScore}</Value>
+              <TypeOfValue>Camera video</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.cameraZoomScore}</Value>
+              <TypeOfValue>Camera zoom</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.cameraSelfieScore}</Value>
+              <TypeOfValue>Camera selfie</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.batteryScore}</Value>
+              <TypeOfValue>Battery</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.audioPlaybackQualityScore}</Value>
+              <TypeOfValue>Audio playback quality</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.audioRecordingQualityScore}</Value>
+              <TypeOfValue>Audio recording quality</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.displayScore}</Value>
+              <TypeOfValue>Display</TypeOfValue>
+            </Circle>
+            <Circle>
+              <Value>{secondResult.performanceScore}</Value>
+              <TypeOfValue>Performance</TypeOfValue>
+            </Circle>
+          </FeatureContainer>
+        </OutcomeContainer>
+      ) : null}
     </div>
   );
 };
